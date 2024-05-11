4617943e0d6cb775c4f467c4b8f3a490bd57b73c
rupatul1995 <rupalichavan578@gmail.com>
2024-05-11 23:53:31 +0530

    update

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 
 // function evenNumbers(){
-//     for( var i=0; i<=10; i+=2){
+//     for( var i=0; i<=10; i++){
+//        if(i%2===0){
 //         console.log(i);
+//        }
+      
 //     }
 // }
 // evenNumbers();
@@ -67,5 +70,29 @@
 
 // Write a JavaScript function that takes a number as input and returns true if it is a prime number, otherwise false.
   
+// 
+// function primeNumber(getnumber){
+//     if (getnumber<2){
+//         return false
+//     }
+//     for(var i=2; i<getnumber/2;i++){
+//         if(getnumber% i===0){
+//             return false
+     
+//         }
+       
+//     }
+//     return true
+// }
+// console.log(primeNumber(7));
 
+// Question 7:
 
+// Create a JavaScript program that calculates the sum of all numbers from 1 to 1000 that are divisible by both 3 and 5.
+// var sum=0;
+// for( var i=1; i<=1000; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log(i)
+//     }
+// }
+    
